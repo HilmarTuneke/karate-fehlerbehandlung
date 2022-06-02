793ef86bfa65102d0f6059d7241d2aced6e60d63
Hilmar Tuneke <hilmar@tuneke.de>
2022-06-02 23:09:32 +0200

    Pausieren und Weitermachen

---
+++ src/main/java/ConsumerRetry.java
@@ -0,0 +1,31 @@
+import io.smallrye.reactive.messaging.kafka.KafkaRecord;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.CompletionStage;
+
+@ApplicationScoped
+public class ConsumerRetry {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerRetry.class);
+
+    @Inject
+    Controller controller;
+
+    @Incoming("kafka")
+    @Retry(maxRetries = 10, delay = 2, delayUnit = ChronoUnit.SECONDS, jitter = 500L)
+    public CompletionStage<Void> consume(KafkaRecord<String, String> record) {
+        try {
+            controller.process(record.getPayload());
+        } catch (Exception e) {
+            LOGGER.error("Oops, something went terribly wrong with " + record, e);
+            throw e;
+        }
+        return record.ack();
+    }
+
+}
